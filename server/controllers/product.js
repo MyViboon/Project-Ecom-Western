1c25aff3373a5e1103c3465c41a8acdf3cd9acff
myviboon <bakpu11@gmail.com>
2025-01-26 17:06:05 +0700

    add listby

--- server/controllers/product.js
+++ server/controllers/product.js
@@ -30,12 +30,39 @@ exports.create = async (req, res) => {
     res.status(500).json({ mesage: "Server ERROR" });
   }
 };
+exports.read = async (req, res) => {
+  try {
+    // code
+    const { id } = req.params;
+    const prodeucts = await prisma.product.findFirst({
+      where: {
+        id: Number(id),
+      },
+      include: {
+        catagory: true,
+        images: true,
+      },
+    });
+    res.send(prodeucts);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ mesage: "Server ERROR" });
+  }
+};
 
 exports.list = async (req, res) => {
   try {
     // code
-
-    res.send("Hello List Product");
+    const { count } = req.params;
+    const prodeucts = await prisma.product.findMany({
+      take: parseInt(count),
+      orderBy: { createdAt: "desc" },
+      include: {
+        catagory: true,
+        images: true,
+      },
+    });
+    res.send(prodeucts);
   } catch (err) {
     console.log(err);
     res.status(500).json({ mesage: "Server ERROR" });
@@ -45,8 +72,37 @@ exports.list = async (req, res) => {
 exports.update = async (req, res) => {
   try {
     // code
+    const { title, description, price, quantity, catagoryId, images } =
+      req.body;
+    // console.log(title, description, price, quantity, images)
 
-    res.send("Hello Update Product");
+    await prisma.image.deleteMany({
+      where: {
+        productId: Number(req.params.id),
+      },
+    });
+
+    const product = await prisma.product.update({
+      where: {
+        id: Number(req.params.id),
+      },
+      data: {
+        title: title,
+        description: description,
+        price: parseFloat(price),
+        quantity: parseInt(quantity),
+        catagoryId: parseInt(catagoryId),
+        images: {
+          create: images.map((item) => ({
+            asset_id: item.asset_id,
+            public_id: item.public_id,
+            url: item.url,
+            secure_url: item.secure_url,
+          })),
+        },
+      },
+    });
+    res.send(product);
   } catch (err) {
     console.log(err);
     res.status(500).json({ mesage: "Server ERROR" });
@@ -56,8 +112,13 @@ exports.update = async (req, res) => {
 exports.remove = async (req, res) => {
   try {
     // code
-
-    res.send("Hello Delete Product");
+    const { id } = req.params;
+    await prisma.product.delete({
+      where: {
+        id: Number(id),
+      },
+    });
+    res.send("Delete Success");
   } catch (err) {
     console.log(err);
     res.status(500).json({ mesage: "Server ERROR" });
@@ -67,8 +128,15 @@ exports.remove = async (req, res) => {
 exports.listby = async (req, res) => {
   try {
     // code
+    const { sort, order, limit } = req.body;
+    // console.log(sort, order, limit);
+    const prodeucts = await prisma.product.findMany({
+      take: limit,
+      orderBy: { [sort]: order },
+      include: { catagory: true },
+    });
 
-    res.send("Hello Listby Product");
+    res.send(prodeucts);
   } catch (err) {
     console.log(err);
     res.status(500).json({ mesage: "Server ERROR" });
--- server/routes/product.js
+++ server/routes/product.js
@@ -7,11 +7,13 @@ const {
   listby,
   searchFilters,
   update,
+  read,
 } = require("../controllers/product");
 
 // locallhost:5000/api/product
 router.post("/product", create);
 router.get("/products/:count", list);
+router.get("/product/:id", read);
 router.put("/product/:id", update);
 router.delete("/product/:id", remove);
 router.post("/productby", listby);
